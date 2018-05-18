81dfc30f147551ab67b0f4e2e6583649f466cc9a
Abdessattar DRIDI <abdessatar.dridi@mgs.com.tn>
2018-05-18 14:46:56 +0100

    dernier niveau du modèle de maturité de Richardson

--- src/Controller/JeuxVideoController.php
+++ src/Controller/JeuxVideoController.php
@@ -2,18 +2,79 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\JeuxVideo;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\FOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Validator\ConstraintViolationList;
 
-class JeuxVideoController extends Controller
+
+class JeuxVideoController extends FOSRestController
 {
+
     /**
-     * @Route("/jeux/video", name="jeux_video")
+     * @Rest\Get("/jeux/videos", name="app_jeux_list")
+     * @View
      */
-    public function index()
+
+    public function listAction()
+
     {
-        return $this->render('jeux_video/index.html.twig', [
-            'controller_name' => 'JeuxVideoController',
-        ]);
+        $jeuxVideos = $this->getDoctrine()
+            ->getRepository(JeuxVideo::class)
+            ->findAll();
+
+        return $jeuxVideos;
+
     }
+
+    /**
+     * @Get(
+     *     path = "/jeux/video/{uid}",
+     *     name = "app_jeux_show",
+     *     requirements = {"uid"="\d+"}
+     * )
+     * @View
+     */
+
+    public function showAction( JeuxVideo $JeuxVideo)
+
+    {
+
+        return $JeuxVideo;
+
+    }
+
+    /**
+     * @Rest\Post(
+     *    path = "/jeux/videos",
+     *    name = "app_jeux_create"
+     * )
+     * @Rest\View(StatusCode = 201)
+     * @ParamConverter("JeuxVideo", converter="fos_rest.request_body")
+     */
+
+    public function createAction(JeuxVideo $JeuxVideo,ConstraintViolationList $violations)
+
+    {
+        if (count($violations)) {
+
+            return $this->view($violations, Response::HTTP_BAD_REQUEST);
+
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($JeuxVideo);
+        $em->flush();
+
+       return $this->view
+       ($JeuxVideo, Response::HTTP_CREATED, ['Location' => $this->generateUrl('app_jeux_show', ['uid' => $JeuxVideo->getUid(), UrlGeneratorInterface::ABSOLUTE_URL])]);
+    }
+
+
+
 }
--- src/Entity/JeuxVideo.php
+++ src/Entity/JeuxVideo.php
@@ -3,9 +3,20 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Hateoas\Configuration\Annotation as Hateoas;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
- * JeuxVideo
+ * @Hateoas\Relation(
+ *      "self",
+ *      href = @Hateoas\Route(
+ *          "app_jeux_show",
+ *          parameters = { "uid" = "expr(object.getUid())" },
+ *          absolute = true
+ *      )
+ * )
+ * JeuxVideouse JMS\Serializer\Annotation as Serializer;
  *
  * @ORM\Table(name="jeux_video", indexes={@ORM\Index(name="ID", columns={"ID"})})
  * @ORM\Entity
@@ -30,7 +41,7 @@ class JeuxVideo
 
     /**
      * @var string
-     *
+     * @Assert\NotBlank
      * @ORM\Column(name="nom", type="string", length=255, nullable=false)
      */
     private $nom;
@@ -39,6 +50,8 @@ class JeuxVideo
      * @var string
      *
      * @ORM\Column(name="possesseur", type="string", length=255, nullable=false)
+     *
+     * @Assert\NotBlank
      */
     private $possesseur;
 
@@ -46,6 +59,7 @@ class JeuxVideo
      * @var string
      *
      * @ORM\Column(name="console", type="string", length=255, nullable=false)
+     * @Assert\NotBlank
      */
     private $console;
 
@@ -53,6 +67,7 @@ class JeuxVideo
      * @var float
      *
      * @ORM\Column(name="prix", type="float", precision=10, scale=0, nullable=false)
+     * @Assert\NotBlank
      */
     private $prix;
 
@@ -60,6 +75,7 @@ class JeuxVideo
      * @var int
      *
      * @ORM\Column(name="nbre_joueurs_max", type="integer", nullable=false)
+     * @Assert\NotBlank
      */
     private $nbreJoueursMax;
 
@@ -67,8 +83,98 @@ class JeuxVideo
      * @var string
      *
      * @ORM\Column(name="commentaires", type="text", length=65535, nullable=false)
+     * @Assert\NotBlank
      */
     private $commentaires;
+
+    public function getUid(): ?int
+    {
+        return $this->uid;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPossesseur(): ?string
+    {
+        return $this->possesseur;
+    }
+
+    public function setPossesseur(string $possesseur): self
+    {
+        $this->possesseur = $possesseur;
+
+        return $this;
+    }
+
+    public function getConsole(): ?string
+    {
+        return $this->console;
+    }
+
+    public function setConsole(string $console): self
+    {
+        $this->console = $console;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getNbreJoueursMax(): ?int
+    {
+        return $this->nbreJoueursMax;
+    }
+
+    public function setNbreJoueursMax(int $nbreJoueursMax): self
+    {
+        $this->nbreJoueursMax = $nbreJoueursMax;
+
+        return $this;
+    }
+
+    public function getCommentaires(): ?string
+    {
+        return $this->commentaires;
+    }
+
+    public function setCommentaires(string $commentaires): self
+    {
+        $this->commentaires = $commentaires;
+
+        return $this;
+    }
 
 
 }
